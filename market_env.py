8af5bca38e8cb17e531079b42df65902e86f3e4a
n-ohara <noboru.ohara2008@gmail.com>
2025-08-19 06:16:39 +0900

    複数トレーダーも可能にする修正

--- market_env.py
+++ market_env.py
@@ -1,42 +1,68 @@
 import numpy as np
+
 class MarketEnvironment:
-    def __init__(self):
+    def __init__(self, num_traders=1):
+        self.num_traders = num_traders
         self.last_price = 100.0
         self.step_count = 0
         self.max_steps = 1000
+        self.positions = [0.0 for _ in range(num_traders)]
+        self.avg_buy_price = [0.0 for _ in range(num_traders)]
         self.action_map = {
-            0: (-1.0, 1),  # 売り強め
-            1: (-0.5, 1),
-            2: (0.0, 1),   # 待機
-            3: (0.5, 1),
-            4: (1.0, 1)    # 買い強め
+            0: -1.0,  # 売り
+            1: 0.0,   # 待機
+            2: 1.0    # 買い
         }
 
     def reset(self):
         self.last_price = 100.0
         self.step_count = 0
-        return np.array([self.last_price], dtype=np.float32)
+        self.positions = [0.0 for _ in range(self.num_traders)]
+        self.avg_buy_price = [0.0 for _ in range(self.num_traders)]
+        return [self._get_initial_state(i) for i in range(self.num_traders)]
 
-    def step(self, action_id):
-        price_delta, volume = self.action_map[action_id]
-        price = self.last_price + price_delta
-        self.last_price = price
+    def step(self, actions):
+        if isinstance(actions, int):
+            actions = [actions]
+        
+        price_delta = sum(self.action_map[a] for a in actions)
+        self.last_price += price_delta
         self.step_count += 1
-        reward = volume * price_delta
+        
+        rewards = []
+        for i, action in enumerate(actions):
+            if action == 2:  # 買い
+                self.positions[i] += 1
+                self.avg_buy_price[i] = (
+                    self.avg_buy_price[i] * (self.positions[i] - 1) + self.last_price
+                    ) / self.positions[i]
+                rewards.append(0.0)
+            elif action == 0 and self.positions[i] > 0:  # 売り
+                profit = (self.last_price - self.avg_buy_price[i]) * self.positions[i]
+                rewards.append(profit)
+                self.positions[i] = 0
+                self.avg_buy_price[i] = 0.0
+            else:
+                rewards.append(0.0)
+        next_states = [self._get_initial_state(i) for i in range(self.num_traders)]
         done = self.step_count >= self.max_steps
-        return np.array([self.last_price], dtype=np.float32), reward, done, {}
+        
+        return next_states, rewards, done, {}
+
+
+    def _get_initial_state(self, trader_id):
+        return np.array([self.last_price, self.positions[trader_id]], dtype=np.float32)
 
     @property
     def action_space(self):
         class Space:
-            n = 5
+            n = 3
             def sample(self):
                 return np.random.randint(0, self.n)
         return Space()
 
-
     @property
     def observation_space(self):
         class Space:
-            shape = (1,)
+            shape = (2,)
         return Space()
